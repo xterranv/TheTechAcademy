d364ec3f11d5f8fad2c2ae487bb28c479ed43af1
xterranv <BeachsideTech@yahoo.com>
2021-03-29 22:56:05 -0400

    Moving Files
    Moving files for re-organization

---
+++ EFConsoleApp/EFConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var cntxt = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Adam" };
+
+                cntxt.Students.Add(student);
+                cntxt.SaveChanges();
+
+                Console.Write("Student entry saved successfully.");
+                FiddleHelper.WriteTable("Student", cntxt.Students.ToList());
+            }
+        }
+    }
+}
